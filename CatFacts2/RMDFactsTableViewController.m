7271244fd121504a1e77b2b0cba2cbe6cb9d8907
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-06-18 14:30:48 -0400

    fix cell height

--- CatFacts2/RMDFactsTableViewController.m
+++ CatFacts2/RMDFactsTableViewController.m
@@ -78,6 +78,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (float)heightForStringDrawing:(NSString *)myString font:(UIFont *)myFont width:(float)myWidth {
+    NSTextStorage *textStorage = [[NSTextStorage alloc]
+                                   initWithString:myString];
+    NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:CGSizeMake(myWidth, FLT_MAX)];
+    NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
+    [layoutManager addTextContainer:textContainer];
+    [textStorage addLayoutManager:layoutManager];
+    [textStorage addAttribute:NSFontAttributeName value:myFont
+                        range:NSMakeRange(0, [textStorage length])];
+    [textContainer setLineFragmentPadding:0.0];
+    (void) [layoutManager glyphRangeForTextContainer:textContainer];
+    return [layoutManager
+            usedRectForTextContainer:textContainer].size.height;
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -105,73 +120,13 @@
         cell.textLabel.textColor = [UIColor whiteColor];
         cell.imageView.image = [UIImage imageNamed:@"CatFace2"];
     }
-//    NSDictionary *nameMap = @{@"imageView" : cell.imageView};
-//    NSArray *heightConstraint = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[imageView(==50)]"
-//                                                                             options:0
-//                                                                             metrics:nil
-//                                                                               views:nameMap];
-//    NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:cell.contentView
-//                                                                       attribute:NSLayoutAttributeHeight
-//                                                                       relatedBy:NSLayoutRelationEqual
-//                                                                          toItem:cell.contentView
-//                                                                       attribute:NSLayoutAttributeWidth
-//                                                                      multiplier:1
-//                                                                        constant:0];
-//    [cell.contentView addConstraints:heightConstraint];
-//    [cell.contentView addConstraint:widthConstraint];
     return cell;
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *str = [self.facts objectAtIndex:indexPath.row];
-    CGSize size = [str sizeWithFont:[UIFont fontWithName:@"Helvetica" size:17] constrainedToSize:CGSizeMake(280, 999) lineBreakMode:NSLineBreakByWordWrapping];
-    return MAX(120, size.height + 30);
+    float stringHeight = [self heightForStringDrawing:[self.facts objectAtIndex:indexPath.row] font:[UIFont fontWithName:@"Helvetica" size:17] width:self.tableView.frame.size.width - 120];
+    return MAX(stringHeight, 120);
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
